0e2361a651c647c46db0b6a4b267d1484a006657
Abhishek P <abhishek.p@piktorlabs.com>
2023-05-02 15:11:55 -0700

    Showing logged in user's name

--- app/src/pages/Home/Home.js
+++ app/src/pages/Home/Home.js
@@ -1,13 +1,19 @@
-import './Home.css'
+import { useMsal } from '@azure/msal-react'
+import { InteractionStatus } from '@azure/msal-browser'
 import Header from '../../modules/Header'
 import LeftSidebar from '../../modules/LeftSidebar'
 import { ReactComponent as ReactLogo } from '../../logo.svg'
+import './Home.css'
 const Home = () => {
+	const { inProgress, accounts } = useMsal()
 	return (
 		<div className='App'>
 			<div className='main-app'>
 				<h1>Main Application in</h1>
 				<ReactLogo className='react-logo' />
+				{inProgress === InteractionStatus.None && (
+					<div>{accounts[0].name} is logged in</div>
+				)}
 			</div>
 			<header className='header-container'>
 				<Header />
