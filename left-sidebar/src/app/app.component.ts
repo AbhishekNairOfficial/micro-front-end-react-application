14845d88f349045ee2711fd854996379e6311ed6
Abhishek P <abhishek.p@piktorlabs.com>
2023-05-04 12:57:47 -0700

    Test

--- left-sidebar/src/app/app.component.ts
+++ left-sidebar/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+const LOGO_IMAGE = require('../assets/logo.png');
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  private LOGO = require('../assets/logo.png');
+  private LOGO = LOGO_IMAGE;
 
   title = 'left-sidebar';
 }
