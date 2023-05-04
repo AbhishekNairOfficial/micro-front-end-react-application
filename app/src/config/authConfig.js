f474a7929f0fecf1cb097808932aa54d82cd9dbe
Abhishek P <abhishek.p@piktorlabs.com>
2023-05-04 12:37:13 -0700

    Updated Home URL

--- app/src/config/authConfig.js
+++ app/src/config/authConfig.js
@@ -3,7 +3,7 @@ import * as msal from '@azure/msal-browser'
 const msalConfig = {
 	auth: {
 		clientId: 'e80c189d-4ac1-473b-bcac-1dc1945553a4',
-		redirectUri: 'http://localhost:3000/home',
+		redirectUri: `${window.location.origin}/home`,
 	},
 }
 
